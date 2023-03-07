f43e63108351d6f27923d7775143b4757957c785
Tejaswini <tejaswini.sp@thundersoft.com>
2023-03-07 11:24:31 +0530

    opearators

---
+++ opear.py
@@ -0,0 +1,42 @@
+#operators
+
+a=20
+b=10
+
+addtion = a + b
+print(addtion)
+sub = a - b
+print(sub)
+mul = a*b
+print(mul)
+div = a/b
+print(div)
+mod = a%b
+print(mod)
+
+c=d=e=50
+print(c)
+
+c,d,e = 1,2,3
+print(d)
+
+m = a > b
+print(m)
+
+bit_and = a & b
+print(bit_and)
+
+bit_or = a | b
+print(bit_or)
+
+
+exp1 = a*b & a+b
+print(exp1)
+
+exp2 = a*b^c+d//2
+print(exp2)
+
+t=2
+exp3 = b>>t&a
+print(exp3)
+
