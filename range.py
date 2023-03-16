b90a68a1bcf04746f3a2a3cbf0b5248ac2a07e71
Tejaswini <tejaswini.sp@thundersoft.com>
2023-03-16 09:56:01 +0530

    range()

---
+++ range.py
@@ -0,0 +1,29 @@
+#range()
+
+'''
+for i in range(1,10,0):   #ValueError: range() arg 3 must not be zero
+    print(i)
+
+
+for i in range(15,2,-1):
+    print(i,end=" ")
+
+
+for i in range(15,2,3):  
+    print(i,end=" ")
+'''
+
+
+for i in range(1,15,20):
+    print(i,end=" ")
+print(" ")
+
+
+for i in range(100):
+    print(i,end=" ")
+print(" ")
+
+
+for i in range(5,25):
+    print(i,end=" ")
+
